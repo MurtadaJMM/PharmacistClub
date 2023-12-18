a7607b50de3b99c0ad6729a89714925af0381118
murtada99pc <murtada99pc@gmail.com>
2023-12-19 00:50:08 +0300

    make some edits on misstakeninformation and decide to makeit firts page on website

--- misstakeninformation.php
+++ misstakeninformation.php
@@ -8,8 +8,9 @@
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 while($row = mysqli_fetch_assoc($result)) {
-                  echo' <div id="displaydiv"> <strong id="title">'.$row['dt'].'</strong><br>
+                  echo' <div id="displaydiv"><div id="tow"> 
                   <span>   by: '.$row['poster'].'</span>
+                  <strong id="title">'.$row['dt'].'</strong></div>
                   <h3 id="dt">'.$row['title'].'</h3>
                   <p>'.$row['content'].'</p>
                   <h3 id="em">by: '.$row['email'].'</h3></div>';
